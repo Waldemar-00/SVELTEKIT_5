adde07859338345afb81821ca980eb47918895eb
uladzimir <01myit@gmail.com>
2025-05-10 16:18:15 +0300

    Routing.

---
+++ kit-app/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to SvelteKit</h1>
+<h2><a href="/about">About</a></h2>
---
+++ kit-app/src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Hello</h1>
+<h2>It is my project for learning SvelteKit above version 5++</h2>
+<h2><a href="/">Home</a></h2>
